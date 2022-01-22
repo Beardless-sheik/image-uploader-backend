3d91efb08734ef6ac95344a1013be608c9f3dd86
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-01-22 18:00:40 +0200

    Update image schema with ueful comments

--- models/image.schema.js
+++ models/image.schema.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+/**
+ * Image Schema
+ */
 const imageSchema = new Schema({
 	cloudinaryURL: String,
 	dateUploaded: String,

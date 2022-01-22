f90099a6215ab5f74dd69293439e2d70e8858f1e
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-01-22 20:35:07 +0200

    Configure routes to make code more modularised by shipping out controllers to their own files

--- routes/images.routes.js
+++ routes/images.routes.js
@@ -1,43 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const imageModel = require('../models/image.schema.js');
-const dateUtility = require('../utils/dateTimeUtility.js');
-
-// define the get images route
-router.post('/', async(req, res) => {
-  const localDateUtil = new dateUtility.localDate();
-  const newImage = new imageModel({
-    cloudinaryURL: req.body.imageUrl,
-    dateUploaded: localDateUtil.getCurrentDate(),
-  })
-  try {
-    if(req.body.imageUrl) {
-      const result = await newImage.save()
-      console.log(result);
-      res.status(201).json({createdData: result});
-    }
-    throw new Error("no image Url passed")
-  }
-  catch(error) {
-    console.log(error);
-    res.status(500).json({ 
-      message: 'Saving image failed!',
-      error: error.message,
-    });
-  }
-})
+const imageControllers = require('../controllers/image.controller.js');
+const createHttpError = require('http-errors');
 
 // define the post images route
-router.get('/', async (req, res) => {
+router.post('/', (req, res, next) => imageControllers.postImage(req,res,next))
+
+// define the get images route
+router.get('/', async (req, res, next) => {
   try {
     const result = await imageModel.find({});;
     res.status(200).json({data: result});
   } catch (error) {
     console.log(error);
-    res.status(500).json({ 
-			message: 'Getting all images failed! See error in response',
-			error: error,
-		});
+    const httpError = createHttpError(500, error);
+    // res.status(500).json({ 
+		// 	message: 'Getting all images failed! See error in response',
+		// 	error: error.message,
+		// });
+    return next(httpError);
   }
 })
 

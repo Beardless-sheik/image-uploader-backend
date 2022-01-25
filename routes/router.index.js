445ee4134ecf06e1e59768a91a517e1c61bd38da
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-01-26 01:06:10 +0200

    Add api/users route to router index

--- routes/router.index.js
+++ routes/router.index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const imageRouter = require('../routes/images.routes');
+const userRouter = require('../routes/user.router');
 const rootRouter = express.Router();
 const app = express();
 
 rootRouter.use('/api/images', imageRouter);
+rootRouter.use('/api/users', userRouter);
 
 module.exports = rootRouter;

cdf1fa0df6d5d11c01f60307cd33cf6502e06f62
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-01-26 02:21:46 +0200

    Reconfigure routes for better modularity

--- routes/router.index.js
+++ routes/router.index.js
@@ -4,7 +4,12 @@ const userRouter = require('../routes/user.router');
 const rootRouter = express.Router();
 const app = express();
 
+//Controller Imports
+const userControllers = require('../controllers/user.controller');
+
 rootRouter.use('/api/images', imageRouter);
 rootRouter.use('/api/users', userRouter);
+rootRouter.post('/api/register', (req, res, next) => userControllers.registerUser(req,res,next));
+rootRouter.post('/api/login', (req, res, next) => userControllers.loginUser(req,res,next));
 
 module.exports = rootRouter;
--- routes/user.router.js
+++ routes/user.router.js
@@ -3,9 +3,6 @@ const router = express.Router();
 const userControllers = require('../controllers/user.controller');
 const userModel = require('../models/user.schema.js');
 
-// define the register route to create a user
-router.post('/register', (req, res, next) => userControllers.registerUser(req,res,next))
-
 // define the get images route
 router.get('/', (req, res, next) => res.status(200).json({ message: "GET operation works on /api/users" })
 ); 
